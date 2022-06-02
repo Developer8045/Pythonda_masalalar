746a353ec79e12064115316ac812d78a8db465aa
Anarboyev Tolibjon <79910234+Developer8045@users.noreply.github.com>
2022-06-03 01:01:52 +0500

    Update and rename sodda_kalkulyator.py to funsimple1.py

---
+++ funsimple1.py
@@ -0,0 +1,18 @@
+# funsimple1
+# masala sharti: Ixtiyoriy sonning 3-darajasini hisoblovchi PowerA3 nomli funksiya hosil qiling.
+# PowerA3 funksiyasi orqali A, B, C haqiqiy sonlarining va D, C butun sonlarining 3-darajasini hisoblovchi programma tuzilsin.
+# 03.06.2022
+
+def PowerA3(x):
+    return x**3
+A = float(input("A = "))
+B = float(input("B = "))
+C = float(input("C = "))
+D = int(input("D = "))
+E = int(input("E = "))
+
+print("A = ", PowerA3(A))
+print("B = ", PowerA3(B))
+print("C = ", PowerA3(C))
+print("D = ", PowerA3(D))
+print("E = ", PowerA3(E))
